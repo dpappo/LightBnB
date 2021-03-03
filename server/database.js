57b41d0a27e4a51bcf10e50d3d7597dfec717a62
Dan Pappo <dpappo@gmail.com>
2021-03-03 14:02:12 -0500

    query functions

--- server/database.js
+++ server/database.js
@@ -17,16 +17,13 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
+  if (!email) {
+    return null;
   }
-  return Promise.resolve(user);
+  return pool.query('SELECT * FROM users WHERE email = $1',[email])
+    .then(res => {
+      return res.rows[0];
+    });
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +33,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  if (!id) {
+    return null;
+  }
+  
+  return pool.query('SELECT * FROM users WHERE id = $1',[id])
+    .then(res => {
+      return res.rows[0];
+    });
+  
+  // return Promise.resolve(users[id]);
 };
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +53,13 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const {name, email, password} = user;
+
+  return pool.query(`INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [name, email, password])
+    .then(res => res.rows[0])
+    .catch(console.error);
 };
 exports.addUser = addUser;
 
@@ -83,10 +92,6 @@ const getAllProperties = function(options, limit = 10) {
   return pool.query(queryString, [limit])
     .then(res => res.rows);
   
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
 };
 exports.getAllProperties = getAllProperties;
 
